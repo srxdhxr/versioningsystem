0b73ae123e61e46f2b5eb686da009a12a7358a9a
srxdhxr <sridharkashyap04@gmail.com>
2024-10-23 11:27:04 -0400

    first commit;

---
+++ flows/steps/step2/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if version is provided
+if [ -z "$1" ]; then
+    echo "Version argument missing! Usage: ./build_and_push.sh <version>"
+    exit 1
+fi
+
+VERSION=$1
+
+# Define your Docker repository (change to your Docker Hub username)
+DOCKER_REPO="srxdhxr/step2"
+
+# Build Docker image with version tag
+docker build -t $DOCKER_REPO:$VERSION .
+
+# Push Docker image to Docker Hub
+docker push $DOCKER_REPO:$VERSION
