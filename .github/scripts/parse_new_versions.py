522f957ba6d02e1b467b2e5b2b956cc5000dd29c
srxdhxr <sridharkashyap04@gmail.com>
2024-10-23 19:44:39 -0400

    update

---
+++ .github/scripts/parse_new_versions.py
@@ -0,0 +1,16 @@
+import sys
+import re
+
+def extract_versions(comment):
+    # Regex to match the format: step1 | [1.0.0] -> [1.0.1]
+    pattern = r'`(.+?)` \| \[(.+?)\] -> \[(.+?)\]'
+    matches = re.findall(pattern, comment)
+    return matches
+
+if __name__ == "__main__":
+    with open(sys.argv[1], 'r') as file:
+        comments = file.read()
+
+    versions = extract_versions(comments)
+    for step, current_version, new_version in versions:
+        print(f"{step} current version: {current_version} -> new version: {new_version}")
